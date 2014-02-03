7d6e09351cc7e7746eb7d22d21feabf6df98d2b6
Raynos <raynos2@gmail.com>
2014-02-03 01:21:23 -0800

    tersify browser.js

--- examples/todomvc/browser.js
+++ examples/todomvc/browser.js
@@ -5,6 +5,4 @@ var main = require("./lib/main.js")
 var State = require("./state.js")
 var App = require("./app.js")
 
-var state = State()
-var surface = document.createElement("div")
-main(state, App, surface)
+main(State(), App, document.body)
