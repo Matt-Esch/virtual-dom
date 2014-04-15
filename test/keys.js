aff7ed63ae7bbe018448640eb26c2ffb2a50a970
Matt-Esch <matt@mattesch.info>
2014-04-15 03:44:40 +0100

    test widgets can be keyed

--- test/keys.js
+++ test/keys.js
@@ -43,7 +43,7 @@ test("keys get reordered", function (assert) {
     var newRoot = patch(rootNode, patches)
     assert.equal(newRoot, rootNode)
 
-    assert.equal(rootNode.childNodes.length, newRoot.childNodes.length)
+    assert.equal(newRoot.childNodes.length, rootNode.childNodes.length)
 
     assert.equal(newRoot.childNodes[7], childNodes[0])
     assert.equal(newRoot.childNodes[3], childNodes[1])
@@ -92,7 +92,7 @@ test("mix keys without keys", function (assert) {
     var newRoot = patch(rootNode, patches)
     assert.equal(newRoot, rootNode)
 
-    assert.equal(rootNode.childNodes.length, newRoot.childNodes.length)
+    assert.equal(newRoot.childNodes.length, rootNode.childNodes.length)
 
     assert.equal(newRoot.childNodes[0], childNodes[1])
     assert.equal(newRoot.childNodes[1], childNodes[2])
@@ -142,7 +142,7 @@ test("missing key gets replaced", function (assert) {
     var newRoot = patch(rootNode, patches)
     assert.equal(newRoot, rootNode)
 
-    assert.equal(rootNode.childNodes.length, newRoot.childNodes.length)
+    assert.equal(newRoot.childNodes.length, rootNode.childNodes.length)
 
     assert.notEqual(newRoot.childNodes[0], childNodes[0])
     assert.equal(newRoot.childNodes[1], childNodes[1])
@@ -152,5 +152,55 @@ test("missing key gets replaced", function (assert) {
     assert.equal(newRoot.childNodes[5], childNodes[5])
     assert.equal(newRoot.childNodes[6], childNodes[6])
     assert.equal(newRoot.childNodes[7], childNodes[7])
+    assert.end()
+})
+
+test("widgets can be keyed", function (assert) {
+    function DivWidget(key, state) {
+        this.key = key
+        this.state = state
+    }
+
+    DivWidget.prototype.init = function () {
+        return render(h("div", this.state))
+    }
+
+    DivWidget.prototype.update = function (rootNode, prev) {
+        if (this.state !== prev.state) {
+            return render(h("div", this.state))
+        }
+    }
+
+    var leftNode = h("div", [
+        new DivWidget("1", "a"),
+        new DivWidget("2", "b"),
+        new DivWidget("3", "c")
+    ])
+
+    var rightNode = h("div", [
+        new DivWidget("3", "c"),
+        new DivWidget("2", "b"),
+        new DivWidget("1", "a")
+    ])
+
+    var rootNode = render(leftNode)
+
+    var childNodes = []
+    for (var i = 0; i < rootNode.childNodes.length; i++) {
+        childNodes.push(rootNode.childNodes[i])
+    }
+
+
+    var patches = diff(leftNode, rightNode)
+    assert.equal(patchCount(patches), 4)    // 1 reorder and 3 update patches
+
+    var newRoot = patch(rootNode, patches)
+    assert.equal(newRoot, rootNode)
+
+    assert.equal(newRoot.childNodes.length, rootNode.childNodes.length)
+
+    assert.equal(newRoot.childNodes[0], childNodes[2])
+    assert.equal(newRoot.childNodes[1], childNodes[1])
+    assert.equal(newRoot.childNodes[2], childNodes[0])
     assert.end()
 })
