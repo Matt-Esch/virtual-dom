d043db471d364e1a2eb60cd77d38441949592ecb
Matt Esch <matt@mattesch.info>
2014-05-04 04:49:51 +0100

    loc refactor in is-vtext

--- vtree/is-vtext.js
+++ vtree/is-vtext.js
@@ -3,9 +3,5 @@ var version = require("./version")
 module.exports = isVirtualText
 
 function isVirtualText(x) {
-    if (!x) {
-        return false;
-    }
-
-    return x.type === "VirtualText" && x.version === version
+    return x && x.type === "VirtualText" && x.version === version
 }
