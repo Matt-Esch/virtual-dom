900d332c2111f172a756989c1fc4a72d2fc19b22
Matt Esch <matt@mattesch.info>
2014-12-21 07:20:44 -0800

    refactor virtual hyperscript

--- virtual-hyperscript/index.js
+++ virtual-hyperscript/index.js
@@ -1,130 +1,125 @@
-var TypedError = require("error/typed")
+'use strict';
 
-var VNode = require("../vnode/vnode.js")
-var VText = require("../vnode/vtext.js")
-var isVNode = require("../vnode/is-vnode")
-var isVText = require("../vnode/is-vtext")
-var isWidget = require("../vnode/is-widget")
-var isHook = require("../vnode/is-vhook")
-var isVThunk = require("../vnode/is-thunk")
+var isArray = require('x-is-array');
 
-var parseTag = require("./parse-tag.js")
-var softSetHook = require("./hooks/soft-set-hook.js")
-var dataSetHook = require("./hooks/data-set-hook.js")
-var evHook = require("./hooks/ev-hook.js")
+var VNode = require('../vnode/vnode.js');
+var VText = require('../vnode/vtext.js');
+var isVNode = require('../vnode/is-vnode');
+var isVText = require('../vnode/is-vtext');
+var isWidget = require('../vnode/is-widget');
+var isHook = require('../vnode/is-vhook');
+var isVThunk = require('../vnode/is-thunk');
 
-var UnexpectedVirtualElement = TypedError({
-    type: "virtual-hyperscript.unexpected.virtual-element",
-    message: "Unexpected virtual child passed to h().\n" +
-        "Expected a VNode / Vthunk / VWidget / string but:\n" +
-        "got a {foreignObjectStr}.\n" +
-        "The parent vnode is {parentVnodeStr}.\n" +
-        "Suggested fix: change your `h(..., [ ... ])` callsite.",
-    foreignObjectStr: null,
-    parentVnodeStr: null,
-    foreignObject: null,
-    parentVnode: null
-})
+var parseTag = require('./parse-tag.js');
+var softSetHook = require('./hooks/soft-set-hook.js');
+var evHook = require('./hooks/ev-hook.js');
 
-module.exports = h
+module.exports = h;
 
 function h(tagName, properties, children) {
-    var childNodes = []
-    var tag, props, key, namespace
+    var childNodes = [];
+    var tag, props, key, namespace;
 
     if (!children && isChildren(properties)) {
-        children = properties
-        props = {}
+        children = properties;
+        props = {};
     }
 
-    props = props || properties || {}
-    tag = parseTag(tagName, props)
+    props = props || properties || {};
+    tag = parseTag(tagName, props);
 
     // support keys
-    if (props.hasOwnProperty("key")) {
-        key = props.key
-        props.key = undefined
+    if (props.hasOwnProperty('key')) {
+        key = props.key;
+        props.key = undefined;
     }
 
     // support namespace
-    if (props.hasOwnProperty("namespace")) {
-        namespace = props.namespace
-        props.namespace = undefined
+    if (props.hasOwnProperty('namespace')) {
+        namespace = props.namespace;
+        props.namespace = undefined;
     }
 
     // fix cursor bug
-    if (tag === "INPUT" &&
+    if (tag === 'INPUT' &&
         !props.namespace &&
-        props.hasOwnProperty("value") &&
+        props.hasOwnProperty('value') &&
         props.value !== undefined &&
         !isHook(props.value)
     ) {
-        props.value = softSetHook(props.value)
+        props.value = softSetHook(props.value);
     }
 
-    transformProperties(props)
+    transformProperties(props);
 
     if (children !== undefined && children !== null) {
-        addChild(children, childNodes, tag, props)
+        addChild(children, childNodes, tag, props);
     }
 
 
-    var node = new VNode(tag, props, childNodes, key, namespace)
+    var node = new VNode(tag, props, childNodes, key, namespace);
 
-    return node
+    return node;
 }
 
 function addChild(c, childNodes, tag, props) {
-    if (typeof c === "string") {
-        childNodes.push(new VText(c))
+    if (typeof c === 'string') {
+        childNodes.push(new VText(c));
     } else if (isChild(c)) {
-        childNodes.push(c)
-    } else if (Array.isArray(c)) {
+        childNodes.push(c);
+    } else if (isArray(c)) {
         for (var i = 0; i < c.length; i++) {
-            addChild(c[i], childNodes, tag, props)
+            addChild(c[i], childNodes, tag, props);
         }
     } else if (c === null || c === undefined) {
-        return
+        return;
     } else {
         throw UnexpectedVirtualElement({
-            foreignObjectStr: JSON.stringify(c),
             foreignObject: c,
-            parentVnodeStr: JSON.stringify({
-                tagName: tag,
-                properties: props
-            }),
             parentVnode: {
                 tagName: tag,
                 properties: props
             }
-        })
+        });
     }
 }
 
 function transformProperties(props) {
     for (var propName in props) {
         if (props.hasOwnProperty(propName)) {
-            var value = props[propName]
+            var value = props[propName];
 
             if (isHook(value)) {
-                continue
+                continue;
             }
 
-            if (propName.substr(0, 5) === "data-") {
-                // add data-foo support
-                props[propName] = dataSetHook(value)
-            } else if (propName.substr(0, 3) === "ev-") {
+            if (propName.substr(0, 3) === 'ev-') {
                 // add ev-foo support
-                props[propName] = evHook(value)
+                props[propName] = evHook(value);
             }
         }
     }
 }
 
 function isChild(x) {
-    return isVNode(x) || isVText(x) || isWidget(x) || isVThunk(x)
+    return isVNode(x) || isVText(x) || isWidget(x) || isVThunk(x);
 }
 
 function isChildren(x) {
-    return typeof x === "string" || Array.isArray(x) || isChild(x)
+    return typeof x === 'string' || isArray(x) || isChild(x);
+}
+
+function UnexpectedVirtualElement(data) {
+    var err = new Error();
+
+    err.type = 'virtual-hyperscript.unexpected.virtual-element';
+    err.message = 'Unexpected virtual child passed to h().\n' +
+        'Expected a VNode / Vthunk / VWidget / string but:\n' +
+        'got a {foreignObjectStr}.\n' +
+        'The parent vnode is {parentVnodeStr}.\n' +
+        'Suggested fix: change your `h(..., [ ... ])` callsite.';
+    err.foreignObject = data.foreignObject;
+    err.parentVnode = data.parentVnode;
+
+    return err;
 }
--- virtual-hyperscript/svg.js
+++ virtual-hyperscript/svg.js
@@ -1,203 +1,53 @@
-var h = require("./index.js")
+'use strict';
 
-// http://www.w3.org/TR/SVGTiny12/attributeTable.html
-var svgAttributes = {
-    "about": true,
-    "accent-height": true,
-    "accumulate": true,
-    "additive": true,
-    "alphabetic": true,
-    "arabic-form": true,
-    "ascent": true,
-    "attributeName": true,
-    "attributeType": true,
-    "bandwidth": true,
-    "baseProfile": true,
-    "bbox": true,
-    "begin": true,
-    "by": true,
-    "calcMode": true,
-    "cap-height": true,
-    "class": true,
-    "content": true,
-    "contentScriptType": true,
-    "cx": true,
-    "cy": true,
-    "d": true,
-    "datatype": true,
-    "defaultAction": true,
-    "descent": true,
-    "dur": true,
-    "editable": true,
-    "end": true,
-    "ev:event": true,
-    "event": true,
-    "externalResourcesRequired": true,
-    "fill": true,
-    "focusHighlight": true,
-    "focusable": true,
-    "font-family": true,
-    "font-stretch": true,
-    "font-style": true,
-    "font-variant": true,
-    "font-weight": true,
-    "from": true,
-    "g1": true,
-    "g2": true,
-    "glyph-name": true,
-    "gradientUnits": true,
-    "handler": true,
-    "hanging": true,
-    "height": true,
-    "horiz-adv-x": true,
-    "horiz-origin-x": true,
-    "id": true,
-    "ideographic": true,
-    "initialVisibility": true,
-    "k": true,
-    "keyPoints": true,
-    "keySplines": true,
-    "keyTimes": true,
-    "lang": true,
-    "mathematical": true,
-    "max": true,
-    "mediaCharacterEncoding": true,
-    "mediaContentEncodings": true,
-    "mediaSize": true,
-    "mediaTime": true,
-    "min": true,
-    "nav-down": true,
-    "nav-down-left": true,
-    "nav-down-right": true,
-    "nav-left": true,
-    "nav-next": true,
-    "nav-prev": true,
-    "nav-right": true,
-    "nav-up": true,
-    "nav-up-left": true,
-    "nav-up-right": true,
-    "observer": true,
-    "offset": true,
-    "origin": true,
-    "overlay": true,
-    "overline-position": true,
-    "overline-thickness": true,
-    "panose-1": true,
-    "path": true,
-    "pathLength": true,
-    "phase": true,
-    "playbackOrder": true,
-    "points": true,
-    "preserveAspectRatio": true,
-    "propagate": true,
-    "property": true,
-    "r": true,
-    "rel": true,
-    "repeatCount": true,
-    "repeatDur": true,
-    "requiredExtensions": true,
-    "requiredFeatures": true,
-    "requiredFonts": true,
-    "requiredFormats": true,
-    "resource": true,
-    "restart": true,
-    "rev": true,
-    "role": true,
-    "rotate": true,
-    "rx": true,
-    "ry": true,
-    "slope": true,
-    "snapshotTime": true,
-    "stemh": true,
-    "stemv": true,
-    "strikethrough-position": true,
-    "strikethrough-thickness": true,
-    "syncBehavior": true,
-    "syncBehaviorDefault": true,
-    "syncMaster": true,
-    "syncTolerance": true,
-    "syncToleranceDefault": true,
-    "systemLanguage": true,
-    "target": true,
-    "timelineBegin": true,
-    "to": true,
-    "transform": true,
-    "transformBehavior": true,
-    "type": true,
-    "typeof": true,
-    "u1": true,
-    "u2": true,
-    "underline-position": true,
-    "underline-thickness": true,
-    "unicode": true,
-    "unicode-range": true,
-    "units-per-em": true,
-    "values": true,
-    "version": true,
-    "viewBox": true,
-    "width": true,
-    "widths": true,
-    "x": true,
-    "x-height": true,
-    "x1": true,
-    "x2": true,
-    "xlink:actuate": true,
-    "xlink:arcrole": true,
-    "xlink:href": true,
-    "xlink:role": true,
-    "xlink:show": true,
-    "xlink:title": true,
-    "xlink:type": true,
-    "xml:base": true,
-    "xml:id": true,
-    "xml:lang": true,
-    "xml:space": true,
-    "y": true,
-    "y1": true,
-    "y2": true,
-    "zoomAndPan": true
-}
+var isArray = require('x-is-array');
 
-var SVG_NAMESPACE = "http://www.w3.org/2000/svg"
+var h = require('./index.js');
 
-module.exports = svg
+
+var isSVGAttribute = require('./is-svg-attribute');
+
+var SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
+
+module.exports = svg;
 
 function svg(tagName, properties, children) {
     if (!children && isChildren(properties)) {
-        children = properties
-        properties = {}
+        children = properties;
+        properties = {};
     }
 
-    properties = properties || {}
+    properties = properties || {};
 
     // set namespace for svg
-    properties.namespace = SVG_NAMESPACE
+    properties.namespace = SVG_NAMESPACE;
 
-    var attributes = properties.attributes || (properties.attributes = {})
+    var attributes = properties.attributes || (properties.attributes = {});
 
     for (var key in properties) {
         if (!properties.hasOwnProperty(key)) {
-            continue
+            continue;
         }
 
-        if (svgAttributes[key] !== true) {
-            continue
+        if (!isSVGAttribute(key)) {
+            continue;
         }
 
-        var value = properties[key]
-        if (typeof value !== "string" &&
-            typeof value !== "number" &&
-            typeof value !== "boolean"
+        var value = properties[key];
+
+        if (typeof value !== 'string' &&
+            typeof value !== 'number' &&
+            typeof value !== 'boolean'
         ) {
-            continue
+            continue;
         }
 
-        attributes[key] = value
+        attributes[key] = value;
     }
 
-    return h(tagName, properties, children)
+    return h(tagName, properties, children);
 }
 
 function isChildren(x) {
-    return typeof x === "string" || Array.isArray(x)
+    return typeof x === 'string' || isArray(x);
 }
