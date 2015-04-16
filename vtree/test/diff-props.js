fe110dc4943b24ef5a003bd3b07d55be11fc2000
Matjaz Gregoric <mtyaka@gmail.com>
2015-04-16 09:59:28 +0200

    Expand the diff-props tests.

--- vtree/test/diff-props.js
+++ vtree/test/diff-props.js
@@ -1,20 +1,58 @@
 var test = require("tape")
 var diffProps = require("../diff-props")
 
-test("add attributes to empty attributes", function (assert) {
-    var propsA = {
-      attributes : {}
-    }
-    var propsB = {
-        attributes : {
-            class : "standard",
-            "e-text" : "custom"
-        }
-    }
+test("adds new properties", function(assert) {
+    var propsA = {}
+    var propsB = {id: 'element-id', className: 'the-class'}
 
-    var diff = diffProps(propsA,propsB)
-    assert.equal(diff.attributes.class, "standard")
-    assert.equal(diff.attributes["e-text"], "custom")
+    var diff = diffProps(propsA, propsB)
+    assert.deepEqual(diff, {
+        id: 'element-id',
+        className: 'the-class'
+    })
+
+    assert.end()
+})
+
+test("removes missing properties", function(assert) {
+    var propsA = {id: 'element-id', className: 'the-class'}
+    var propsB = {}
+
+    var diff = diffProps(propsA, propsB)
+    assert.deepEqual(diff, {
+        id: undefined,
+        className: undefined
+    })
+
+    assert.end()
+})
+
+test("recursively adds new properties", function(assert) {
+    var propsA = {attributes: {}}
+    var propsB = {attributes: {'data-custom': 'custom', class: 'the-class'}}
+
+    var diff = diffProps(propsA, propsB)
+    assert.deepEqual(diff, {
+        attributes: {
+            'data-custom': 'custom',
+            class: 'the-class'
+        }
+    })
+
+    assert.end()
+})
+
+test("recursively removes missing properties", function(assert) {
+    var propsA = {attributes: {'data-custom': 'custom', class: 'the-class'}}
+    var propsB = {attributes: {}}
+
+    var diff = diffProps(propsA, propsB)
+    assert.deepEqual(diff, {
+        attributes: {
+            'data-custom': undefined,
+            class: undefined
+        }
+    })
 
     assert.end()
 })
