9dde0a1625bcc6e757e2489367bd76d4fcd18392
Raynos (Jake Verbaten) <raynos2@gmail.com>
2015-06-03 11:02:25 -0700

    Update update-widget.js

--- vdom/update-widget.js
+++ vdom/update-widget.js
@@ -4,7 +4,7 @@ module.exports = updateWidget
 
 function updateWidget(a, b) {
     if (isWidget(a) && isWidget(b)) {
-        if ("name" in a && "name" in b) {
+        if ("id" in a && "id" in b) {
             return a.id === b.id
         } else {
             return a.init === b.init
