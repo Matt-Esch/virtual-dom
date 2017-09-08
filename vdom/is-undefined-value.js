69e637d111815f2aeba9a0b22b75de0969b48b98
alexeygrinko-xo <alexey.grinko@crossover.com>
2017-09-08 12:31:14 +0300

    Renamve isUndefinedHook to isUndefinedValue

--- vdom/is-undefined-value.js
+++ vdom/is-undefined-value.js
@@ -1,8 +1,8 @@
 //Magic value to map keys with a value of undefined to in JSON form since JSON
 //won't preserve those.  This is necessary because in patches, the removal of
 //a property is represented by the property name mapped to undefined
-function isUndefinedHook(value) {
+function isUndefinedValue(value) {
     return value === undefined || value === '____UnDeFiNeD____';
 }
 
-module.exports = isUndefinedHook;
+module.exports = isUndefinedValue;
